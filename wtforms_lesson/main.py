d6dde47b0ef0a369864f8e3196b7263e271c3f56
Jack Lee <jack.lee.1981@pm.me>
2020-12-29 17:05:18 -0300

    Finish WTForms lesson.

--- wtforms_lesson/main.py
+++ wtforms_lesson/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from forms import InfoForm
+from flask import Flask, render_template, session, redirect, url_for, flash
+from forms import InfoForm, MyForm
 
 app = Flask(__name__)
 app.config[
@@ -16,6 +16,28 @@ def index():
         form.breed.data = ""
 
     return render_template("index.html", form=form, breed=breed)
+
+
+@app.route("/my-form", methods=["GET", "POST"])
+def my_form():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        session["breed"] = form.breed.data
+        session["neutered"] = form.neutered.data
+        session["mood"] = form.mood.data
+        session["food_choice"] = form.food_choice.data
+        session["feedback"] = form.feedback.data
+        flash("Thank you!!!")
+
+        return redirect(url_for("thank_you"))
+
+    return render_template("my_form.html", form=form)
+
+
+@app.route("/thank-you")
+def thank_you():
+    return render_template("thank_you.html")
 
 
 if "__main__" == __name__:
