f20d05bd5c34eecbf6d0794313170ff3f13e4e35
Jack Lee <jack.lee.1981@pm.me>
2021-01-01 09:05:02 -0300

    Add Moment.js to localize dates.

--- company-blog/project/__init__.py
+++ company-blog/project/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from project.error_pages.handlers import error_pages
 import os
 
@@ -15,6 +16,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 Migrate(app=app, db=db)
+
+moment = Moment(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
